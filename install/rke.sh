4aa4b0cdc59a3803eb8c00b9e1248e50fb1bdfc2
Corwin Harris <git@corwin.betabeetle.com>
2020-11-10 18:11:56 -0500

    Bumped RKE version.

--- install/rke.sh
+++ install/rke.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-RKE_URL="https://github.com/rancher/rke/releases/download/v1.1.9/rke_linux-amd64"
+RKE_URL="https://github.com/rancher/rke/releases/download/v1.2.2/rke_linux-amd64"
 TARGET_RKE=/usr/local/bin/rke
 
 sudo wget -q $RKE_URL -O $TARGET_RKE
